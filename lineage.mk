aa2fdc0884d82856694ab9a556594f2f23e0b430
deadman96385 <seanhoyt963@gmail.com>
2017-02-07 13:29:04 -0600

    Remove unified support
    klimtlte is its own device

--- lineage.mk
+++ lineage.mk
@@ -15,3 +15,12 @@ PRODUCT_NAME := lineage_klimtltecan
 PRODUCT_DEVICE := klimtltecan
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-T705W
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=klimtltevl-user 5.0.2 LRX22G T705WVLU1BOD7 release-keys \
+    PRIVATE_BUILD_DESC="samsung/klimtltevl/klimtltecan:5.0.2/LRX22G/T705WVLU1BOD7:user/release-keys" \
+    PRODUCT_MODEL=SM-T705W \
+    PRODUCT_NAME=klimtltecan \
+    PRODUCT_DEVICE=klimtltecan \
+    TARGET_DEVICE=klimtltecan
